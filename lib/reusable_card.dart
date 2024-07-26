5e26e9170d5ff1c3cd09594915de90ec075c561b
Yonathan Cruz <yonathancruz2019@gmail.com>
2024-07-25 19:44:25 -0600

    add onPress props and wrap inside gesture detector component

--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -2,20 +2,27 @@ import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
   const ReusableCard(
-      {super.key, required this.colour, required this.cardChild});
+      {super.key,
+      required this.colour,
+      required this.cardChild,
+      required this.onPress});
 
   final Color colour;
+  final VoidCallback onPress;
   final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: colour,
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        margin: const EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: colour,
+        ),
+        child: cardChild,
       ),
-      child: cardChild,
     );
   }
 }
