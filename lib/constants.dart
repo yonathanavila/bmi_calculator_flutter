e56243e02be9bbd1e58c6f7dbfc81a4e53ea2de8
Yonathan Cruz <yonathancruz2019@gmail.com>
2024-07-25 19:43:23 -0600

    add constants file

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 20.0,
+  color: Color(0xff231918)
+);
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColour = Color(0xff904a42);
+const kInactiveCardColour = Color(0xff775652);
