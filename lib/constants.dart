500fab1cc2422529889ffcd588ea483062d80dab
Yonathan Cruz <yonathancruz2019@gmail.com>
2024-07-25 22:01:23 -0600

    add label subtitle style

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,6 +5,9 @@ const kLabelTextStyle = TextStyle(
   color: Color(0xff231918)
 );
 
+const kLabelSubtitleStyle = TextStyle(fontSize: 50.0,
+fontWeight: FontWeight.w900);
+
 const kBottomContainerHeight = 80.0;
 const kActiveCardColour = Color(0xff904a42);
 const kInactiveCardColour = Color(0xff775652);
